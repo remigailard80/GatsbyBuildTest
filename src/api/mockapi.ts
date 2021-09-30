7e7fa5518693fb285b7580f47380f5b21b022082
daangn <daangn@hanhyejuui-MacBookPro.local>
2021-09-30 18:36:45 +0900

    api settings

--- src/api/mockapi.ts
+++ src/api/mockapi.ts
@@ -1,4 +1,4 @@
 export async function getPostAPI () {
-    const result = await fetch('http://127.0.0.1:8080/post').then(res => res.json())
+    const result = await fetch('http://127.0.0.1:8090/post').then(res => res.json())
     return result
 }
